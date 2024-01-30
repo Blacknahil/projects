fbfa28d8a77f286f844ca7caf9fec84cd7c72834
Nahom Garefo <harobina28@gmail.com>
2024-01-30 16:47:04 +0300

    database

--- Tutor_Linkup/backend/config/db.js
+++ Tutor_Linkup/backend/config/db.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-const db = "mongodb+srv://Tutor_Linkup1:3fE2Ny4oWf9pwPSh@cluster0.qhs3xy1.mongodb.net/";
-
+const db ="mongodb+srv://yordanos:1234@cluster1.isdg4lt.mongodb.net/?retryWrites=true&w=majority/";
 // Log the MongoDB URI for debugging purposes
 console.log("MongoDB URI:", db);
 
