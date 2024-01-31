e3b7a6e90b8b6216cb472d94e168f2f3e293bfc4
Nahom Garefo <harobina28@gmail.com>
2024-02-01 00:38:37 +0300

    merging for user_identity

--- src/user_identity/script.js
+++ src/user_identity/script.js
@@ -1,24 +1,32 @@
+// document.querySelector('#form').addEventListener('submit',async(event)=>{
+//     event.preventDefault();
+//     const role=document.querySelector('input[name="user_type"]:checked').value;
+//     await submitUserIdentity(role);
+// });
+
+// const submitUserIdentity=async(role)=>{
+//     const response = await fetch('http://localhost:4078/user/identity',{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             role:role
+//         })
+//     });
+//     if (response.ok){
+//         window.location.href=response.url;
+//     }
+//     else{
+//         console.error(response.status,response.statusText);
+//         console.log(response);
+//     }
+// }
+
+// approach two that is saving the role of the user in the local stroage and then using it in the next page
 document.querySelector('#form').addEventListener('submit',async(event)=>{
     event.preventDefault();
     const role=document.querySelector('input[name="user_type"]:checked').value;
-    await submitUserIdentity(role);
-});
-
-const submitUserIdentity=async(role)=>{
-    const response = await fetch('http://localhost:4078/user/identity',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            role:role
-        })
-    });
-    if (response.ok){
-        window.location.href=response.url;
-    }
-    else{
-        console.error(response.status,response.statusText);
-        console.log(response);
-    }
-}
+    localStorage.setItem('role',role);
+    window.location.href='../../html,css,Js for registration and tutor profie/sign up with email form/signup.html';
+})
